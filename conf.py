624e4726b060c0a402cbca7d08c098f652e4f0f0
Marcos Suzuki <marcossuzuki@gmail.com>
2018-12-14 00:50:47 -0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,12 +14,12 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.append(os.path.abspath('.'))
+#sys.path.append(os.path.abspath('.'))
 
-from sphinx_rtd_theme import __version__
+#from sphinx_rtd_theme import __version__
 
 # -- Project information -----------------------------------------------------
 
